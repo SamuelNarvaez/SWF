166ced093faa34c2dfade4c331484806166c3a27
SamuelNarvaez <samueljnarvaez@gmail.com>
2022-04-06 23:49:44 +0200

    added swf class

--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -1,4 +1,8 @@
-import numpy as np
+from trimesh import *
+import numpy as np 
+import plotly.graph_objects as go
+import plotly.offline as pyo
+from plotly.subplots import make_subplots
 
 def faces_to_edges(faces, return_index=False):
     """
@@ -54,3 +58,61 @@ def get_third_neighbors(adj):
 
 def check_sum_to_1():
     pass
+
+def weights3D(mesh, weights, name=''):  
+    x = mesh.vertices[:,0]
+    y = mesh.vertices[:,1]
+    z = mesh.vertices[:,2]
+    i = mesh.faces[:,0]
+    j = mesh.faces[:,1]
+    k = mesh.faces[:,2]
+
+    verts = mesh.vertices
+    faces = mesh.faces
+
+    fig = make_subplots(
+              rows=1, cols=1, 
+              subplot_titles=(f'{name} Level {mesh.level} 3D Mesh'),
+              horizontal_spacing=0.02,
+              specs=[[{"type": "scene"}]*1])  
+
+    #plot surface triangulation
+    tri_vertices = verts[faces]
+    Xe = []
+    Ye = []
+    Ze = []
+    for T in tri_vertices:
+        Xe += [T[k%3][0] for k in range(4)] + [ None]
+        Ye += [T[k%3][1] for k in range(4)] + [ None]
+        Ze += [T[k%3][2] for k in range(4)] + [ None]
+
+
+
+    fig.add_trace(go.Mesh3d(x=x, y=y, z=z, 
+                            i=i, j=j, k=k, colorscale='matter_r' ,
+                            colorbar_len=0.85,
+                            colorbar_x=0.97,
+                            colorbar_thickness=20,
+                            intensity=weights,
+                            text=weights,
+                            intensitymode='vertex',
+                            flatshading=True), 1, 1)
+    lighting = dict(ambient=0.5,
+                    diffuse=1,
+                    fresnel=4,        
+                    specular=0.5,
+                    roughness=0.05,
+                    facenormalsepsilon=0)
+    lightposition=dict(x=100,
+                       y=100,
+                       z=10000)
+    
+    fig.data[0].update(lighting=lighting,
+                       lightposition=lightposition)                         
+
+
+    fig.update_layout(width=1000, height=600, font_size=10)
+    fig.update_scenes(camera_eye_x=1.45, camera_eye_y=1.45, camera_eye_z=1.45);
+    fig.update_scenes(xaxis_visible=False, yaxis_visible=False,zaxis_visible=False )
+
+    fig.show()
---
+++ swf.py
@@ -0,0 +1,84 @@
+import numpy as np
+from trimesh import *
+
+class SWF():
+    def __init__(self,base='oct',n=3,l=0):
+        '''
+        base : string or Trimesh 
+            string points to some predefined base mesh
+            Trimesh sets the base mesh manually
+        
+        n : int
+            finest mesh subdivision level
+        l : int
+            truncation level
+        
+        '''
+        presets = {'oct': Trimesh(np.array([[1,0,0],[-1,0,0],[0,1,0],[0,-1,0],[0,0,1],[0,0,-1]]),np.array([[1,2,4],[1,3,4],[3,0,4],[0,2,4],[1,3,5],[3,0,5],[0,2,5],[2,1,5]]),ALPHA=1/2,BETA=1/8,GAMMA=-1/24),
+                   }
+        if type(base) is str:
+            self.base = presets[base]
+        else: 
+            self.base = base
+        self.n = int(n)
+        self.l = int(l)
+        self.meshes = []
+        current = self.base
+        for i in range(self.n):
+            result = current.subdivide()
+            self.meshes.append(result)
+            current = result
+        self.Ps = [m.filters[0] for m in self.meshes]
+        self.Qs = [m.filters[1] for m in self.meshes]
+        self.As = [m.filters[2] for m in self.meshes]
+        self.Bs = [m.filters[3] for m in self.meshes]
+        self.phis = [self.phi(j) for j in range(self.n)]
+        self.psis = [self.psi(j) for j in range(self.n)]
+        self.phi2s = [self.phi2(j) for j in range(self.n)]
+        self.psi2s = [self.psi2(j) for j in range(self.n)]
+        
+    def phi(self,j):
+        '''
+        computes the direct scaling function as (P_n*...*P_j+2*P_j+1)
+        
+        j : int [0,n-1] 
+            level
+        '''
+        result = np.eye(self.Ps[j].shape[1])
+        for P in self.Ps[j:]:
+            result = P @ result
+        return result
+    def psi(self,j): 
+        '''
+        computes the direct wavelet as (P_n*...*P_j+2*Q_j+1)
+        
+        j : int [0,n-1] 
+            level
+        '''
+        result = self.Qs[j]
+        for P in self.Ps[j+1:]:
+            result = P @ result
+        return result
+    def phi2(self,j):
+        '''
+        computes the dual scaling function as (A_j+1*A_j+2*...*A_n)
+        
+        j : int [0,n-1] 
+            level
+        '''
+        result = np.eye(self.As[-1].shape[1])
+        for A in self.As[j:][::-1]:
+            result = A @ result
+        return result
+    def psi2(self,j):
+        '''
+        computes the dual wavelet as (B_j+1*A_j+2*...*A_n)
+        
+        j : int [0,n-1] 
+            level
+        '''
+        result = np.eye(self.As[-1].shape[1])
+        for A in self.As[j+1:][::-1]:
+            result = A @ result
+        result = self.Bs[j] @ result
+        return result
